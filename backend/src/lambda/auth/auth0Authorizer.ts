4f1cf1731ee77a598a9832b218b1945fc4b8cdb8
nhadd <aoducnha29493@gmail.com>
2022-07-15 22:38:18 +0700

    implement authentication

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -3,16 +3,12 @@ import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-x20p6tdd.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -58,17 +54,48 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  verify(token, getKey, { algorithms: ['HS256'] }, function (err, decodeed: object) {
+    if (err) {
+      logger.error('Error when decode JWT token: ', {error: err.message})
+      throw new Error('Invalid JWT token!')
+    }
+    const jwtPayload = jwt.payload
+    if (decodeed['sub'] !== jwt.payload.sub
+      || decodeed['iss'] !== jwtPayload.iss
+      || decodeed['iat'] !== jwtPayload.iat
+      || decodeed['exp'] !== jwtPayload.exp) {
+        logger.error('Incorrect JWT token!')
+        throw new Error('Incorrect JWT token!')
+    }
+  })
+  return jwt.payload
+}
+
+function getKey(header, callback) {
+  const jwksClient = require('jwks-rsa');
+  const client = jwksClient({
+    jwksUri: jwksUrl
+  })
+
+  client.getSigningKey(header.pid, function (err, key) {
+    if (err) {
+      logger.error('Error when get Public Key: ', {error: err.message})
+    }
+    var signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
 }
 
 function getToken(authHeader: string): string {
-  if (!authHeader) throw new Error('No authentication header')
+  if (!authHeader) {
+    logger.error('No authentication header.')
+    throw new Error('No authentication header')
+  } 
 
-  if (!authHeader.toLowerCase().startsWith('bearer '))
+  if (!authHeader.toLowerCase().startsWith('bearer ')) {
+    logger.error('Invalid authentication header.')
     throw new Error('Invalid authentication header')
+  }
 
   const split = authHeader.split(' ')
   const token = split[1]
